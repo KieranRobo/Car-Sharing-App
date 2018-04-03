760c350dc8e0a22ed32723c8b604187103f2b4a2
Kieran Robertson <kieran.robertson.2013@uni.strath.ac.uk>
2018-04-03 17:53:18 +0100

    Example showing AJAX usage connecting to database

---
+++ control/ajax-handler.php
@@ -0,0 +1,12 @@
+<?php
+include ("database.php");
+
+if ($_GET['data'] == "rides") {
+    $rides = $conn->query("SELECT * FROM `rides`");
+
+    $rows = array();
+    while ($r = $rides->fetch_array()) {
+        $rows[] = $r;
+    }
+    print json_encode($rows);
+}
