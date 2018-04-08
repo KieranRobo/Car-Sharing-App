65714aa4c8fa188f21b7a49c9e013f798775cd2c
Cammy <cameron.lamberth.2015@uni.strath.ac.uk>
2018-04-08 16:03:04 +0100

    fixed swipe again..

--- js/swipe.js
+++ js/swipe.js
@@ -1,14 +1,18 @@
-$(document).on('pageinit', function () {
+$(document).one('pageinit', function () {
 
     $('#findcontent').one("swipeleft", function () {
+        console.log("to activerides");
         $.mobile.changePage("activerides.html");
     });
 
     $('#findcontent').one("swiperight", function () {
+        console.log("to create rides");
         $.mobile.changePage("create-ride.html");
     });
 
     $('#activecontent').one("swiperight", function () {
+        console.log("to findrides");
+
         $.mobile.changePage("findride.html");
     });
 });
