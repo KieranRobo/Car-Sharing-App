11d3994a1403422beafed793abadb174b8bb10b0
Cammy <cameron.lamberth.2015@uni.strath.ac.uk>
2018-04-08 16:48:15 +0100

    Added swipe for create ride. Added logout button

---
+++ js/logout.js
@@ -0,0 +1,15 @@
+$(document).one('pageinit', function () {
+    $('.logout').one("click", function () {
+        $.ajax({
+            url: 'control/login-handler.php',
+            data: "logout=true",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                if(data === "success")
+                    $.mobile.navigate("login.html", {transition: "slide"})
+            }
+
+        });
+    });
+});
