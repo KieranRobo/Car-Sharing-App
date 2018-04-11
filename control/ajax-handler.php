149a18c2b31e0487e4f338900c68a01c18e04f53
Cammy <cameron.lamberth.2015@uni.strath.ac.uk>
2018-04-11 11:57:00 +0100

    Implemented my rides to show only your rides

--- control/ajax-handler.php
+++ control/ajax-handler.php
@@ -12,6 +12,34 @@ if (isset($_GET['data']) && $_GET['data'] == "rides") {
     print json_encode($rows);
 }
 
+if(isset($_SESSION['userID']) && isset($_GET['data']) && $_GET['data'] == 'myrides'){
+    $result = $conn->query("SELECT ride_id FROM passengers WHERE user_id = ".$_SESSION['userID']);
+    $rideID = array();
+    while ($row = $result->fetch_assoc()){
+        $rideID[] = $row;
+    }
+    $rides = array();
+    $result->close();
+    foreach($rideID as $id){
+        $result = $conn->query("SELECT `time`, setoff_location FROM rides WHERE id=".$id['ride_id']);
+        while ($row = $result->fetch_assoc())
+            $rides[] = $row;
+    }
+    $location = array();
+    $result->close();
+    foreach ($rides as $ride) {
+        $result = $conn->query("SELECT address FROM locations WHERE id =".$ride['setoff_location']);
+        while ($row = $result->fetch_assoc())
+            $location[] = $row;
+    }
+    $data = array();
+    for($i = 0; $i<sizeof($rides); $i++){
+        $data[] = array('time' => $rides[$i]['time'], 'location' => $location[$i]['address']);
+    }
+    print json_encode($data);
+
+}
+
 if (isset($_GET['data']) && $_GET['data'] == "vehicles") {
     $rides = $conn->query("SELECT * FROM `vehicles` WHERE `owner` = {$_SESSION['userID']}");
 
@@ -55,3 +83,4 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         echo $seatsTotal-$num;
     }
 }
+
---
+++ js/my-rides.js
@@ -0,0 +1,24 @@
+$(document).one('pageinit', function () {
+    $.getJSON("control/ajax-handler.php?data=myrides", function(data){
+        for(var i=0; i<data.length; i++){
+            $('#my-rides-table').find('tbody').append("<tr onclick='showInfo();' id='rowMy"+i+"'><td id='timeMy"+i+"'></td>" +
+                "<td id='fromMy"+i+"'>");
+            $('#timeMy' + i).append(data[i].time);
+            $('#fromMy' + i).append(data[i].location);
+            // updateTo(data[i]['dest_location'], i);
+            // updateSeatsLeft(data[i]['vehicle'], i);
+            if(i%2 === 0)
+                $('#rowMy'+i).css('background-color', '#d3d3d3');
+
+
+
+        }
+        $('#my-rides-table').table("refresh");
+        console.log(data[0]);
+    });
+
+});
+
+function showInfo() {
+    $.mobile.changePage('ride-details.html', 'pop', true, true);
+}
